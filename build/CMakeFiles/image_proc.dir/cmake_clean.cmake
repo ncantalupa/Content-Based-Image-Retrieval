2f2ec5f5498a11988e253f47da93bfe2bfdeec02
ncantalupa <ncantalupa@gmail.com>
2025-01-28 14:33:47 -0500

    Setting Up Environment

---
+++ build/CMakeFiles/image_proc.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_proc.dir/csv_util/csv_util.cpp.o"
+  "CMakeFiles/image_proc.dir/csv_util/csv_util.cpp.o.d"
+  "CMakeFiles/image_proc.dir/image_proc.cpp.o"
+  "CMakeFiles/image_proc.dir/image_proc.cpp.o.d"
+  "image_proc"
+  "image_proc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_proc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
